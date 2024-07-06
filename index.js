7f8bfa06031b0216cb6db0b23117b155bb711333
naneen <nazneenbanosiddiqui@gmail.com>
2024-07-06 10:34:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+/*var num1 = 12;
+var num2 = 2;
+
+alert(num1+num2)
+
+let a = 2;
+
+document.write(2*2*2)*/
+
+
+//.........................................................................................
+/*var a= parseInt(prompt("enter 1st no."))
+var b= parseInt(prompt("enter 2nd no."))
+
+alert(a+b)*/
+
+//...........................................................................................
+//var a= parseInt(prompt("enter any number"))
+//alert(a*a)
+//alert(a*a*a)
+
+//var h= parseInt(prompt("enter any number"))
+//alert(1/2*a*h)
+
+//if else condition.........
+
+//......................................................................................................
+//et num = parseInt(prompt("enter the number"))
+//console.log(num);
+/*if(num>0)
+{
+ alert("positive")   
+}
+else{
+    alert("negative")
+}*/
+
+//...................nasted......................................................................
+//let num = parseInt(prompt("enter the number"))
+//console.log(num);
+/*if(num%2!=0)
+{
+  alert("odd")  
+}
+else
+{
+    alert("even")
+}*/
+
+/*if(num>0){
+    alert("+ve num")
+}
+else if(num<0)
+    {
+    alert("-ve num")
+}
+else{
+    alert("nuteral")
+}*/
+
+//let num = parseInt(prompt("enter the number"))
+//console.log(num);
+/*if(num>=100 && num<=500)
+{
+    alert("5 star*10")
+}
+else if(num>500 && num<=1000)
+    {
+    alert("shopping")
+}
+else if(num>1000 && num<=10000){
+    alert("trip")
+}
+else{
+   alert("paese nahi he ham ghar par rahenge") 
+}*/
+
+/*let num = parseInt(prompt("enter the number"))
+console.log(num);
+
+if(num%2==0){
+    alert(num*num)
+}
+else{
+    alert(num*num*num)
+}*/
+
+//..............nasted.................................
+
+/*var age = parseInt(prompt("enter your age"))
+
+if(age>=18){
+    var ind=parseInt(prompt("press 1 for "))
+    if(ind==1){
+        alert("eligible")
+    }
+    else{
+        alert("not eligible")
+    }
+ }
+ else{
+    alert("pehle bade ho jao")
+ }*/
+
+ //....................switch case.....................
+var a = parseInt(prompt("we have seven faucly name enter any number 1-7"))
+alert(a)
+switch(a){
+    case 1:
+        alert("qasim sir")
+        break;
+    case 2:
+        alert("ashish sir")
+        break;
+    case 3: 
+        alert("rajesh sir") 
+        break;
+    case 4:
+        alert("uzair sir")
+        break;     
+
+default :
+
+        alert("no faculty")
+}
+
+
+
+
+
+
+
+
